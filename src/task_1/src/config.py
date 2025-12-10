96da96d55ee415499606eb4584420659776bc0fe
Лопухов Евгений Олегович <eo_lopuhov@controlsystems.ru>
2025-12-10 13:16:55 +0300

    fix: config task 1 fixed

--- src/task_1/src/config.py
+++ src/task_1/src/config.py
@@ -1,19 +1,22 @@
 import configparser
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
-config.read('../config.ini')
+config_path = Path(__file__).resolve().parent.parent / "config.ini"
 
-MIN_SIZE_POOL = config['database'].getint('min_size_pool')
-MAX_SIZE_POOL = config['database'].getint('max_size_pool')
-TIMEOUT = config['database'].getint('max_size_pool')
+config = configparser.ConfigParser()
+config.read(config_path)
+
+MIN_SIZE_POOL = config["database"].getint("min_size_pool")
+MAX_SIZE_POOL = config["database"].getint("max_size_pool")
+TIMEOUT = config["database"].getint("max_size_pool")
 
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT"))
+DB_PORT = int(os.getenv("DB_PORT", "9000"))
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
